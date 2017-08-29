60ddb3685c84b00752cfea2f11767a14f18555d6
Michael Xusheng Chen <michael.xschen@gmail.com>
2017-08-29 16:33:12 +0800

    for dbg

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -1045,184 +1045,142 @@ e1000_receive_iov(NetClientState *nc, const struct iovec *iov, int iovcnt)
     return size;
 }
 
-// static void *push_to_guest(void *nc){
+static void *push_to_guest(void *nc){
     
 
+    while(1){
+        pthread_mutex_lock(&list_lock);
+        if (list_len < pushed_len){
+            pthread_mutex_unlock(&list_lock);
+            sched_yield();
+            continue; 
+        }
 
-
-//     while(1){
-//         pthread_mutex_lock(&list_lock);
-//         if (list_len < pushed_len){
-//             pthread_mutex_unlock(&list_lock);
-//             sched_yield();
-//             continue; 
-//         }
-
-//         while(list_len >= pushed_len){
-//             E1000State *s = qemu_get_nic_opaque((*NetClientState)nc);
-//             PCIDevice *d = PCI_DEVICE(s);
-//             struct e1000_rx_desc desc;
-//             dma_addr_t base;
-//             unsigned int n, rdt;
-//             uint32_t rdh_start;
-//             uint16_t vlan_special = 0;
-//             uint8_t vlan_status = 0;
-//             uint8_t min_buf[MIN_BUF_SIZE];
-//             struct iovec min_iov;
+        while(list_len >= pushed_len){
+            E1000State *s = qemu_get_nic_opaque((*NetClientState)nc);
+            PCIDevice *d = PCI_DEVICE(s);
+            struct e1000_rx_desc desc;
+            dma_addr_t base;
+            unsigned int n, rdt;
+            uint32_t rdh_start;
+            uint16_t vlan_special = 0;
+            uint8_t vlan_status = 0;
+            uint8_t min_buf[MIN_BUF_SIZE];
+            struct iovec min_iov;
             
-//             uint8_t *filter_buf = iov_list[pushed_len]->iov_base;
-//             size_t size = iov_size(iov_list[pushed_len], 1);
+            uint8_t *filter_buf = iov_list[pushed_len]->iov_base;
+            size_t size = iov_size(iov_list[pushed_len], 1);
             
-//             size_t iov_ofs = 0;
-//             size_t desc_offset;
-//             size_t desc_size;
-//             size_t total_size;
-
-//             if (!e1000x_hw_rx_enabled(s->mac_reg)) {
-//                 pushed_len++;
-//                 continue;
-//             }
-
-//             /* Pad to minimum Ethernet frame length */
-//             if (size < sizeof(min_buf)) {
-//                 iov_to_buf(iov_list[pushed_len], 1, 0, min_buf, size);
-//                 memset(&min_buf[size], 0, sizeof(min_buf) - size);
-//                 e1000x_inc_reg_if_not_full(s->mac_reg, RUC);
-//                 min_iov.iov_base = filter_buf = min_buf;
-//                 min_iov.iov_len = size = sizeof(min_buf);
-//                 iovcnt = 1;
-//                 iov_list[pushed_len] = &min_iov;
-//             } else if (iov_list[pushed_len]->iov_len < MAXIMUM_ETHERNET_HDR_LEN) {
-//                 /* This is very unlikely, but may happen. */
-//                 iov_to_buf(iov_list[pushed_len], 1, 0, min_buf, MAXIMUM_ETHERNET_HDR_LEN);
-//                 filter_buf = min_buf;
-//             }
-
-//             /* Discard oversized packets if !LPE and !SBP. */
-//             if (e1000x_is_oversized(s->mac_reg, size)) {
-//                 pushed_len++;
-//                 continue;
-//             }
-
-//             if (!receive_filter(s, filter_buf, size)) {
-//                 pushed_len++;
-//                 continue;
-//             }
-
-//             if (e1000x_vlan_enabled(s->mac_reg) &&
-//                 e1000x_is_vlan_packet(filter_buf, le16_to_cpu(s->mac_reg[VET]))) {
-//                 vlan_special = cpu_to_le16(lduw_be_p(filter_buf + 14));
-//                 iov_ofs = 4;
-//                 if (filter_buf == iov_list[pushed_len]->iov_base) {
-//                     memmove(filter_buf + 4, filter_buf, 12);
-//                 } else {
-//                     iov_from_buf(iov_list[pushed_len], 1, 4, filter_buf, 12);
-//                     while (iov_list[pushed_len]->iov_len <= iov_ofs) {
-//                         iov_ofs -= iov_list[pushed_len]->iov_len;
-//                         iov++;
-//                     }
-//                 }
-//                 vlan_status = E1000_RXD_STAT_VP;
-//                 size -= 4;
-//             }
-
-//             rdh_start = s->mac_reg[RDH];
-//             desc_offset = 0;
-//             total_size = size + e1000x_fcs_len(s->mac_reg);
-//             if (!e1000_has_rxbufs(s, total_size)) {
-//                     set_ics(s, 0, E1000_ICS_RXO);
-//                     return -1;
-//             }
-
-//             //
-//             do {
-//                 //desc_size: size to copy this round. 
-//                 desc_size = total_size - desc_offset;
-//                 if (desc_size > s->rxbuf_size) {
-//                     desc_size = s->rxbuf_size;
-//                 }
-//                 //xs: rx_desc_base: return the address of the rx_base
-//                 //: RDH: rx_descriptors_head; Question: what is head? seems a count 
+            size_t iov_ofs = 0;
+            size_t desc_offset;
+            size_t desc_size;
+            size_t total_size;
 
 
-//                 base = rx_desc_base(s) + sizeof(desc) * s->mac_reg[RDH];
-//                 //xs: read the dst from the memory
-//                 pci_dma_read(d, base, &desc, sizeof(desc));
+            struct iovec* iov = iov_list[pushed_len];
+            int iovcnt = 1; 
+
+
+            rdh_start = s->mac_reg[RDH];
+            desc_offset = 0;
+            total_size = size + e1000x_fcs_len(s->mac_reg);
+            if (!e1000_has_rxbufs(s, total_size)) {
+                    set_ics(s, 0, E1000_ICS_RXO);
+                    pushed_len++; 
+                    if (pushed_len >= iov_list_maxlen)
+                    pushed_len -= iov_list_maxlen; 
+                    continue;
+            }
+
+   
+            do {
+                //desc_size: size to copy this round. 
+                desc_size = total_size - desc_offset;
+                if (desc_size > s->rxbuf_size) {
+                    desc_size = s->rxbuf_size;
+                }
+                //xs: rx_desc_base: return the address of the rx_base
+                //: RDH: rx_descriptors_head; Question: what is head? seems a count 
+
+
+                base = rx_desc_base(s) + sizeof(desc) * s->mac_reg[RDH];
+                //xs: read the dst from the memory
+                pci_dma_read(d, base, &desc, sizeof(desc));
                 
 
-//                 desc.special = vlan_special;
-//                 desc.status |= (vlan_status | E1000_RXD_STAT_DD);
-//                 if (desc.buffer_addr) {
-//                     //xs: size: the total size of the iovs. 
-//                     //desc_offset: number of bits copied already. 
-//                     if (desc_offset < size) {
-//                         size_t iov_copy;
-//                         hwaddr ba = le64_to_cpu(desc.buffer_addr);
-//                         size_t copy_size = size - desc_offset;
-//                         if (copy_size > s->rxbuf_size) {
-//                             copy_size = s->rxbuf_size;
-//                         }
-//                         do {
-//                             iov_copy = MIN(copy_size, iov->iov_len - iov_ofs);
-//                             //xs: copy the content to the buffer. 
-//                             pci_dma_write(d, ba, iov->iov_base + iov_ofs, iov_copy);
-//                             copy_size -= iov_copy;
-//                             ba += iov_copy;
-//                             iov_ofs += iov_copy;
-//                             if (iov_ofs == iov->iov_len) {
-//                                 iov++;
-//                                 iov_ofs = 0;
-//                             }
-//                         } while (copy_size);
-//                     }
-//                     desc_offset += desc_size;
-//                     desc.length = cpu_to_le16(desc_size);
-//                     if (desc_offset >= total_size) {
-//                         desc.status |= E1000_RXD_STAT_EOP | E1000_RXD_STAT_IXSM;
-//                     } else {
-//                         /* Guest zeroing out status is not a hardware requirement.
-//                            Clear EOP in case guest didn't do it. */
-//                         desc.status &= ~E1000_RXD_STAT_EOP;
-//                     }
-//                 } else { // as per intel docs; skip descriptors with null buf addr
-//                     DBGOUT(RX, "Null RX descriptor!!\n");
-//                 }
-//                 pci_dma_write(d, base, &desc, sizeof(desc));
+                desc.special = vlan_special;
+                desc.status |= (vlan_status | E1000_RXD_STAT_DD);
+                if (desc.buffer_addr) {
+                    //xs: size: the total size of the iovs. 
+                    //desc_offset: number of bits copied already. 
+                    if (desc_offset < size) {
+                        size_t iov_copy;
+                        hwaddr ba = le64_to_cpu(desc.buffer_addr);
+                        size_t copy_size = size - desc_offset;
+                        if (copy_size > s->rxbuf_size) {
+                            copy_size = s->rxbuf_size;
+                        }
+                        do {
+                            iov_copy = MIN(copy_size, iov->iov_len - iov_ofs);
+                            //xs: copy the content to the buffer. 
+                            pci_dma_write(d, ba, iov->iov_base + iov_ofs, iov_copy);
+                            copy_size -= iov_copy;
+                            ba += iov_copy;
+                            iov_ofs += iov_copy;
+                            if (iov_ofs == iov->iov_len) {
+                                iov++;
+                                iov_ofs = 0;
+                            }
+                        } while (copy_size);
+                    }
+                    desc_offset += desc_size;
+                    desc.length = cpu_to_le16(desc_size);
+                    if (desc_offset >= total_size) {
+                        desc.status |= E1000_RXD_STAT_EOP | E1000_RXD_STAT_IXSM;
+                    } else {
+                        /* Guest zeroing out status is not a hardware requirement.
+                           Clear EOP in case guest didn't do it. */
+                        desc.status &= ~E1000_RXD_STAT_EOP;
+                    }
+                } else { // as per intel docs; skip descriptors with null buf addr
+                    DBGOUT(RX, "Null RX descriptor!!\n");
+                }
+                pci_dma_write(d, base, &desc, sizeof(desc));
 
-//                 //xs: wrap. 
-//                 if (++s->mac_reg[RDH] * sizeof(desc) >= s->mac_reg[RDLEN])
-//                     s->mac_reg[RDH] = 0;
-//                 /* see comment in start_xmit; same here */
-//                 if (s->mac_reg[RDH] == rdh_start ||
-//                     rdh_start >= s->mac_reg[RDLEN] / sizeof(desc)) {
-//                     DBGOUT(RXERR, "RDH wraparound @%x, RDT %x, RDLEN %x\n",
-//                            rdh_start, s->mac_reg[RDT], s->mac_reg[RDLEN]);
-//                     set_ics(s, 0, E1000_ICS_RXO);
-//                     return -1;
-//                 }
-//             } while (desc_offset < total_size);
+                //xs: wrap. 
+                if (++s->mac_reg[RDH] * sizeof(desc) >= s->mac_reg[RDLEN])
+                    s->mac_reg[RDH] = 0;
+                /* see comment in start_xmit; same here */
+                if (s->mac_reg[RDH] == rdh_start ||
+                    rdh_start >= s->mac_reg[RDLEN] / sizeof(desc)) {
+                    DBGOUT(RXERR, "RDH wraparound @%x, RDT %x, RDLEN %x\n",
+                           rdh_start, s->mac_reg[RDT], s->mac_reg[RDLEN]);
+                    set_ics(s, 0, E1000_ICS_RXO);
+                    return -1;
+                }
+            } while (desc_offset < total_size);
 
-//             e1000x_update_rx_total_stats(s->mac_reg, size, total_size);
+            e1000x_update_rx_total_stats(s->mac_reg, size, total_size);
 
-//             n = E1000_ICS_RXT0;
-//             if ((rdt = s->mac_reg[RDT]) < s->mac_reg[RDH])
-//                 rdt += s->mac_reg[RDLEN] / sizeof(desc);
-//             if (((rdt - s->mac_reg[RDH]) * sizeof(desc)) <= s->mac_reg[RDLEN] >>
-//                 s->rxbuf_min_shift)
-//                 n |= E1000_ICS_RXDMT0;
+            n = E1000_ICS_RXT0;
+            if ((rdt = s->mac_reg[RDT]) < s->mac_reg[RDH])
+                rdt += s->mac_reg[RDLEN] / sizeof(desc);
+            if (((rdt - s->mac_reg[RDH]) * sizeof(desc)) <= s->mac_reg[RDLEN] >>
+                s->rxbuf_min_shift)
+                n |= E1000_ICS_RXDMT0;
 
-//             set_ics(s, 0, n);
-//             pushed_len++;
-//             if (pushed_len >= iov_list_maxlen)
-//                 pushed_len -= iov_list_maxlen; 
+            set_ics(s, 0, n);
+            pushed_len++;
+            if (pushed_len >= iov_list_maxlen)
+                pushed_len -= iov_list_maxlen; 
 
-//         }
+        }
 
-//         pthread_mutex_unlock(&list_lock);
-//         sched_yield();
+        pthread_mutex_unlock(&list_lock);
+        sched_yield();
 
-//     }
-// }
+    }
+}
 
 
 
